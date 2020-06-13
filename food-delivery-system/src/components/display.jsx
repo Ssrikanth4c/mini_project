04e760ac7aeb14b5ba7a85f4569cb653906a141f
srikanth4c <srikanthbinf@gmail.com>
2020-06-13 15:20:06 +0530

    Updated: display the list of restaurants with different food items

--- food-delivery-system/src/components/display.jsx
+++ food-delivery-system/src/components/display.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import { connect } from 'react-redux' 
-
-const Display=({restName})=>{
+const Display=({data, addToCardHandler})=>{
+    console.log(data)
     return(
-        <div>
-            sree
-            {restName?.map(ele=>ele.restaurantName)}
+        <div className='card'>
+            <img className='card-img-top' style={{height:'100%', width:'100%'}} src={data.img} alt='Food_Item_Img'/>
+                <h5 className='mt-1 card-title'>{data.FoodItem}</h5>
+            
+    <b>Price: <span className='text-danger'>&#8377;</span>{data.price}</b>
+            
+            <button className='btn btn-primary'
+                onClick={()=>{addToCardHandler(data.id)}}
+            >
+                Add to Cart
+            </button>          
         </div>
     )
 }
-const mapStateToProps=state=>({
-    restName: state.restData,
-    search:state.search
-})
-export default connect(mapStateToProps)(Display)
+export default Display
